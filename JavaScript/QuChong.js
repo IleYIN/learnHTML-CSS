573a3156ffa77e731121527236c96955c69f9f90
yinyiliang <yiliang.yin@etu.isae-ensma.fr>
2019-08-05 00:18:53 +0200

    carousefigue

---
+++ JavaScript/QuChong.js
@@ -0,0 +1,35 @@
+
+// 方法一：
+var arr1 =[1,2,2,2,3,3,3,4,5,6],
+    arr2 = [];
+for(var i = 0,len = arr1.length; i< len; i++){
+    if(arr2.indexOf(arr1[i]) < 0){
+        arr2.push(arr1[i]);
+    }
+}
+document.write(arr2); // 1,2,3,4,5,6
+
+
+//统计字符串”aaaabbbccccddfgh”中字母个数或统计最多字母数。
+var str = "aaaabbbccccddfgh";
+var obj  = {};
+for(var i=0;i<str.length;i++){
+    var v = str.charAt(i);
+    if(obj[v] && obj[v].value == v){
+        obj[v].count = ++ obj[v].count;
+    }else{
+        obj[v] = {};
+        obj[v].count = 1;
+        obj[v].value = v;
+    }
+}
+for(key in obj){
+    document.write(obj[key].value +'='+obj[key].count+' '); // a=4  b=3  c=4  d=2  f=1  g=1  h=1 
+}
+
+//写一个function，清除字符串前后的空格。（兼容所有浏览器）
+function trim(str) {
+    if (str && typeof str === "string") {
+        return str.replace(/(^\s*)|(\s*)$/g,""); //去除前后空白符
+    }
+}
---
+++ JavaScript/js/tools.js
@@ -0,0 +1,75 @@
+ /* 
+            speed 正负值应该由函数内部来处理
+            attr 要执行的动画样式 比如left top width height
+            callback 回调函数 动画执行完毕以后执行
+        */
+        function move(obj, attr, target, speed, callback) {
+
+            //关闭上一个定时器
+            clearInterval(obj.timer);
+
+            /* 判断速度的正负值 */
+            var current = parseInt(getStyle(obj, attr));
+            if (current > target) {
+                speed = -speed;
+            }
+
+
+            //向执行动画的对象中添加一个timer属性，用来保存它自己的定时器的标识
+            obj.timer = setInterval(function () {
+                // console.log(getStyle(obj,attr)); 如果不一开始给设置好left和top 很有可能默认是auto！！！就无法获取数值了！！！
+
+                var oldValue = parseInt(getStyle(obj, attr));
+                // console.log(oldValue);
+
+
+                //获取box1的原来的left值
+                // var oldValue = obj.offsetLeft;
+                //有可能修改各种，不止offsetLeft，比如height，故用更常用的之前定义过的getSytle
+                var newValue = oldValue + speed;
+
+                if ((speed > 0 && newValue > target) || (speed < 0 && newValue < target)) {
+                    newValue = target;
+                }
+                // console.log(newValue);
+
+
+                /* 
+                    obj.style.left = newValue + "px";
+                    attr是变量，不能用. 要用[]
+                */
+                obj.style[attr] = newValue + "px";
+
+
+                //元素移到target 时停止动画
+                if (newValue === target) {
+                    clearInterval(obj.timer);
+                    // if (callback != null) {
+                    //     callback();
+                    // }
+                    callback && callback();
+                }
+            }, 30);
+
+        }
+
+
+
+        function getStyle(obj, name) {
+            // getComputedStyle(obj, null).name;//cannot write like this, name is a variable
+
+            if (window.getComputedStyle) {
+                /*
+                    if browser has this method,
+                    here we use window.getComputedStyle which is an attribute
+                    not use getComputedStyle which is a variable, if variable is not found, it will raise un error
+                */
+
+                return getComputedStyle(obj, null)[name];
+            } else {
+                return obj.currentStyle[name];
+            }
+
+            //or 
+            // return window.getComputedStyle ? getComputedStyle(obj, null)[name] : obj.currentStyle[name];
+        }
